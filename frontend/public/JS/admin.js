ea6756022b4d5841618e40d5ac1e024cf8810c62
Sandra Chege <chegemwihaki88@gmail.com>
2023-11-01 14:46:15 +0300

    Front end code

---
+++ frontend/public/JS/admin.js
@@ -0,0 +1,10 @@
+var personiconbtn = document.querySelector(".personiconbtn");
+var dropdownContent = document.querySelector(".dropdown-content");
+
+personiconbtn.addEventListener("click", () => {
+  if (dropdownContent.style.display === 'none') {
+    dropdownContent.style.display === 'block'
+  }
+  
+});
+
